171b2f2d9ec1768f76a875e4d103c9e2b08fe0af
Kel-D-Coder <okolochibundu887@gmail.com>
2024-09-21 14:32:19 +0100

    Integrated emailjs

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const tabLinks = document.getElementsByClassName('tab-links');
 const tabContents = document.getElementsByClassName('tab-contents');
 const sideMenu = document.getElementById('sidemenu')
-
+const successMsg = document.querySelector('.success-msg')
+const errorMsg = document.querySelector('.error-msg')
 
 const opentab = (tabname) => {
 
@@ -23,3 +24,23 @@ const  openmenu = () => {
 const  closemenu = () => {
     sideMenu.style.right = "-200px"
 }
+
+const sendMail = (e) => {
+    e.preventDefault()
+    let params = {
+        name: document.getElementById('name').value,
+        email: document.getElementById('email').value,
+        message: document.getElementById('message').value
+    }
+
+    emailjs.send("service_fhjdast", "template_spmm5jx", params).then(() => {
+        document.querySelector('form').reset()
+        successMsg.textContent = "Email Sent successully"
+        successMsg.style.color = 'green'
+
+    }).catch(() => {
+        errorMsg.textContent = "There was an error"
+    })
+}
+
+document.querySelector('form').addEventListener('submit', sendMail);
