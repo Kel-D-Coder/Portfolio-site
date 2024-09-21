cf21a8232c3db02f97c9f4d3b95b633b8eadc94e
Kel-D-Coder <okolochibundu887@gmail.com>
2024-09-21 12:12:46 +0100

    finished the UI

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const tabLinks = document.getElementsByClassName('tab-links');
 const tabContents = document.getElementsByClassName('tab-contents');
+const sideMenu = document.getElementById('sidemenu')
 
 
 const opentab = (tabname) => {
@@ -13,4 +14,12 @@ const opentab = (tabname) => {
     }
     event.currentTarget.classList.add('active-link')
     document.getElementById(tabname).classList.add("active-tab");
+}
+
+const  openmenu = () => {
+    sideMenu.style.right = "0"
+}
+
+const  closemenu = () => {
+    sideMenu.style.right = "-200px"
 }
